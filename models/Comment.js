fc048c1c88517169ac72e446f3ddf04a46c76def
Kelvin Chao <kelvinchao2011@gmail.com>
2022-05-31 19:19:38 -0700

    added validators

--- models/Comment.js
+++ models/Comment.js
@@ -3,26 +3,30 @@ const dateFormat = require("../utils/dateFormat");
 
 const ReplySchema = new Schema(
   {
+    // set custom id to avoid confusion with parent comment _id
     replyId: {
       type: Schema.Types.ObjectId,
-      default: () => new Types.ObjectId(),
+      default: () => new Types.ObjectId()
     },
     replyBody: {
       type: String,
+      trim: true,
+      required: true
     },
     writtenBy: {
       type: String,
+      required: true,
     },
     createdAt: {
       type: Date,
       default: Date.now,
       get: createdAtVal => dateFormat(createdAtVal)
-    },
+    }
   },
   {
     toJSON: {
-      getters: true,
-    },
+      getters: true
+    }
   }
 );
 
